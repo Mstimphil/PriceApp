1787308d8b68b2edf43f65e1385a72bcb1003094
Mstimphil <mstimphil@my.yearupunited.org>
2025-09-26 00:21:18 -0400

    float

--- src/main/java/com/pluralsight/PriceApp.java
+++ src/main/java/com/pluralsight/PriceApp.java
@@ -2,6 +2,11 @@ package com.pluralsight;
 
 public class PriceApp {
     public static void main(String[] args) {
+int count = 77;
+float unitPrice = 8.97f;
+float taxRate = 0.825f;
+float totalCost;
+
 
     }
 }
